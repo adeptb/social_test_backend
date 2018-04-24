7d1bf5d2a9b8d73d84238b22d1abc73ba6e8644b
aa.velichko <aa.velichko@jet.msk.su>
2018-04-24 17:41:13 +0300

    little fix

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,15 +4,11 @@ const authService   = require('./../services/AuthService');
 const create = async function(req, res){
     res.setHeader('Content-Type', 'application/json');
     const body = req.body;
-    if(!body.unique_key && !body.email && !body.phone){
-        return ReE(res, 'Please enter an email or phone number to register.');
-    } else if(!body.password){
+    if(!body.password){
         return ReE(res, 'Please enter a password to register.');
     }else{
         let err, user;
-
         [err, user] = await to(authService.createUser(body));
-
         if(err) return ReE(res, err, 422);
         return ReS(res, {message:'Successfully created new user.', user:user.toWeb(), token:user.getJWT()}, 201);
     }
